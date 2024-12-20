36feeb869cbbc5ec60bedd4e8b1c46c1c0e498d9
khanabdurrahman27 <79714622+khanabdurrahman27@users.noreply.github.com>
2024-12-20 22:51:53 +0530

    writing-auth-middleware

---
+++ src/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const app = express();
+
+//Handle Auth Middleware for all the GET POST,.... requests
+
+const { adminAuth, userAuth } = require('./middlewares/auth');
+
+app.get("/admin", adminAuth);
+
+app.post("/user/login",(req,res)=>{
+  res.send("User logged in successfully");
+})
+ 
+app.get("/user",userAuth, (req,res)=>{
+  res.send("User Data Sent");
+});
+
+app.get("/admin/getAllData", (req, res)=>{
+    res.send('All Data Sent');
+  });
+
+app.get("/admin/deleteUser", (req, res)=>{
+    res.send('Delete a user');
+  });
+
+app.use("/hello",(req, res) => {
+  res.send("Hello hello hello!");
+})
+
+app.use("/test",(req, res, next) => {
+  //Route Handler 1
+  console.log("Handling the route user!!");
+  // res.send("Hello from the server!"); // when 1 response not send then it will not send it will not send respond 2 by using next then it will send response 2;
+  next();
+},
+(req, res)=>{
+//Route Handler 2
+console.log("Handling the route user 2!!");
+res.send("2nd Response");
+})
+
+app.listen(3000, ()=>{
+  console.log("Server is successfully listening on port 3000....");
+});
